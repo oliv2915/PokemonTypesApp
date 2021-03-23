9c60a8a46b80af95b03727c6438eb8cd5bfe9b25
Brian Oliver II <brian.d.oliver@icloud.com>
2021-03-23 16:31:24 -0400

    redesign of the pay layout to help with better styling, refactored js to better handle access to elements

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const pokemonTypesBaseURL = "https://pokeapi.co/api/v2/type/";
 const pokemonBaseURL = "https://pokeapi.co/api/v2/pokemon/";
 
-const pokemonTypesListView = document.querySelector("#type-list"); // allows access to the pokemonTypesList on the html page
+
 const pokemonListView = document.querySelector("#pokemon-list"); // allows access to the pokemonList on the html page
-const pokemonDataSection = document.querySelector("#pokemon-data");
+const pokemonDataView = document.querySelector("#pokemon-data");
+pokemonDataView.style.display = "none";
+pokemonListView.parentElement.style.display = "none"; // sets the pokemon list card-body element to hidden, this removes the ghost box seen if set to block
 
 let currentlyActiveTypeLink; // used to keep track of which pokemen type was clicked in typeListLinkCicked()
 let currentlyActivePokemonLink; // used to keep track of which pokemen type was clicked in currentlyActivePokemonLink()
@@ -39,95 +41,126 @@ function checkForOkResponse(response) { // Used to handle all errors from fetch 
 }
 
 function displayPokemonTypesList(typesList) { // loads pokemonTypes into the pokemoneTypesListView
+    const pokemonTypesListView = document.querySelector("#type-list")
     let pokemonTypes = typesList.results; // array of pokemonTypes
-    let header = document.createElement("h4"); // create h6 element
-    header.setAttribute("class", "h4 text-center") // add class of h6
-    header.innerText = "Types of"; // add some text
-    pokemonTypesListView.appendChild(header); // add to the list view
     createListLinks(pokemonTypes, pokemonTypesListView, typeListLinkClicked); // call createListLinks passing in our list of pokemonTypes, where the list should show, and a click event handler
 }
 
-function displayPokemonList(pokemonList) { // load pokemon ino the pokemonListView
+function displayPokemonList(pokemonData) { // load pokemon ino #pokemon-list
+    // displays the #pokemon-list, this was disabled by default on line 6
+    pokemonListView.parentElement.style.display = "block";
+    /*
+        pokemonData is an arry of objects. Our specific data is deeply nested. To access the name for one pokemon, we would need to use pokemonData.pokemon[0].pokemon
+        We need to create our own array which will hold objs of pokemon. Addess one pokemon, we type pokemon.name
+    */
+    let pokemonArray = pokemonData.pokemon;
+    let pokemon = [];
+
+    // check to see if the pokemonArray is empty
+    // here is where we process the data to be passed into the view
+    if (pokemonArray != 0) {
+        // forEach obj in the pokemonArray
+        pokemonArray.forEach((obj) => {
+            // push the obj found at key pokemon onto our own pokemon array
+            pokemon.push(obj.pokemon);
+        })
+    } else {
+        pokemonListView.parentElement.style.display = "none"; // hide the #pokemon-list element
+    }
+    
+    
+
     // this while statement handles the possiblity for there to already be a list of pokemon in the list view
     // if there are any, remove them from the list
-    // end result is that the list view will be empty
     while(pokemonListView.firstChild) { // while a list item is present
         pokemonListView.removeChild(pokemonListView.firstChild); // remove it
     }
-
-    // here is where we start to create a new list view
-    let header = document.createElement("h4"); // we need a header
-    header.setAttribute("class", "h4 text-center") // add class of h6
-    header.innerText = "Pokemon"; // add some text to it
-    pokemonListView.appendChild(header); // and add it to the top of the list view
-
-    /*
-        Inside our pokemonList object is key of pokemon which holds an array of pokemon with keys of pokemon. It is deeply nested.
-        This means to get the name of the first pokeman, we will need to use pokemonList.pokemon[0].pokemon.name
-    */
-    let pokemon = []; // place holder array for our pokemon
-    /*
-        
-    */
-    pokemonList.pokemon.forEach((pokeman) => {
-        pokemon.push(pokeman.pokemon);
-    })
-
+    // add pokemon to the #pokemon-list and add click event
     createListLinks(pokemon, pokemonListView, pokemonListLinkClicked);
     
 }
 
 function displayPokemonData(pokemon) {
     // references to the pokemon data we need to display
-    let name = pokemon.name
-    let stats = pokemon.stats;
-    let abilities = pokemon.abilities;
-    let moves = pokemon.moves;
-    let image = pokemon.sprites.other["official-artwork"]["front_default"]
+    const name = pokemon.name
+    const types = pokemon.types
+    const stats = pokemon.stats;
+    const abilities = pokemon.abilities;
+    const moves = pokemon.moves;
+    const image = pokemon.sprites.other["official-artwork"]["front_default"]
 
     // references to the view elements where the data needs to be displayed
-    let imgElem = pokemonDataSection.querySelector("#pokemon-image");
-    let nameElem = pokemonDataSection.querySelector("#pokemon-name");
-    let statsElem = pokemonDataSection.querySelector("#pokemon-stats");
-    let abilitiesElem = pokemonDataSection.querySelector("#pokemon-abilities");
-    let movesElem = pokemonDataSection.querySelector("#pokemon-moves");
+    const pokemonNameElement = document.querySelector("#pokemon-name");
+    const pokemonTypeElement = document.querySelector("#pokemon-type");
+    const pokemonImageElement = document.querySelector("#pokemon-image");
+
     // sets the name and image
-    imgElem.src = image;
-    nameElem.innerText = name;
-    nameElem.style.textTransform = "capitalize"
-
-    displayStats(stats, statsElem);
-    displayAbilities(abilities, abilitiesElem);
+    pokemonImageElement.src = image;
+    pokemonNameElement.innerText = name;
+    pokemonNameElement.style.textTransform = "capitalize"
     
-    //TODO: Still need to define
-    //    stats => statsElem
-    //    abilities => abilitiesElem
+    displayStats(stats);
 
-    // will clear the view of any previously loaded pokemon data
-    // while(pokemonDataSection.firstChild) {
-    //     pokemonDataSection.removeChild(pokemonDataSection.firstChild);
-    // }
+    
+    // displayAbilities(abilities, abilitiesElem);
 }
 
 function displayAbilities(abilities, parentElement) {
     
 }
 
-function displayStats(stats, parentElement) { // HP: 40 | ATTACK: 80 | DEFENSE: 35
-    let hp = stats[0];
-    let attack = stats[1];
-    let defense = stats[2];
-
-    let statString = `HP: ${hp["base_stat"]} | ATTACK: ${attack["base_stat"]} | DEFENSE: ${defense["base_stat"]}`
-
-    parentElement.innerText = statString;
+function displayStats(pokemonStats) {
+    pokemonDataView.style.display = "flex";
+    // stats elements
+    const hpElement = document.querySelector("#hp-stat");
+    const attackElement = document.querySelector("#attack-stat");
+    const defenseElement = document.querySelector("#defense-stat");
+    const specialAttackElement = document.querySelector("#special-attack-stat");
+    const specialDefenseElement = document.querySelector("#special-defense-stat");
+    const speedElement = document.querySelector("#speed-stat");
+    // placeholders to the data that we need to display
+   
+    let stats = [];
+    // forEach stat, build our own object for that stat
+    pokemonStats.forEach(stat => {
+        stats.push({
+            name: stat.stat.name,
+            base_stat: stat.base_stat,
+            effort: stat.effort,
+            url: stat.stat.url
+        })
+    })
+    
+    stats.forEach((stat) => {
+        switch (stat.name) {
+            case "hp":
+                hpElement.innerText = `HP: ${stat.base_stat}`;
+                break;
+            case "attack":
+                attackElement.innerText = `ATK: ${stat.base_stat}`;
+                break;
+            case "defense":
+                defenseElement.innerText = `DEF: ${stat.base_stat}`;
+                break;
+            case "special-attack":
+                specialAttackElement.innerText = `SP-ATK: ${stat.base_stat}`;
+                break;
+            case "special-defense":
+                specialDefenseElement.innerText = `SP-DEF: ${stat.base_stat}`;
+                break;
+            case "speed":
+                speedElement.innerText = `SPD: ${stat.base_stat}`;
+            default:
+                break;
+        }
+    })
 }
 
 function createListLinks(list, parentElement, clickEventHandler) { // create anchor tags for the provided list, adds the anchor tag to the parentElement, and assigns an event listner to each anchor tag
     // console.log(list);
     list.forEach((item) => { // forEach item in the list
         let a = document.createElement("a"); // create an anchor tag
-        a.setAttribute("class", "nav-link text-center") // give it the class of nav-link
+        a.setAttribute("class", "list-group-item list-group-item-action") // give it the class of nav-link
         a.innerText = item.name; // set the innerText to that of the item.name
         a.style.textTransform = "capitalize"; // capitalize the first letter of innerText
         parentElement.appendChild(a) // add the anchor tag to the parentElement that this list should be seen in
@@ -151,12 +184,12 @@ function typeListLinkClicked(event) {
     let newlyClickedLink = event.target; // provides a reference to the newlyClickedLink
     
     if (currentlyActiveTypeLink === undefined) { // check to see if undefined
-        newlyClickedLink.setAttribute("class", "nav-link text-center active"); // add the active class and keep the nav-link class for the newlyClickedLink
+        newlyClickedLink.setAttribute("class", "list-group-item list-group-item-action active"); // add the active class and keep the nav-link class for the newlyClickedLink
         currentlyActiveTypeLink = newlyClickedLink; // sets the currentlyActiveTypeLink to be that of the newlyClickedClink
         queryURL = pokemonTypesBaseURL + newlyClickedLink.innerHTML; // add newlyClickedLink innerHTML to the queryURL
     } else if (currentlyActiveTypeLink !== newlyClickedLink) { // and if it does not match
-        newlyClickedLink.setAttribute("class", "nav-link text-center active"); // add the active class and keep the nav-link class for the newlyClickedLink
-        currentlyActiveTypeLink.setAttribute("class", "nav-link text-center"); // removes  active class and keeps the nav-link class on the now old currentlyActiveTypeLink
+        newlyClickedLink.setAttribute("class", "list-group-item list-group-item-action active"); // add the active class and keep the nav-link class for the newlyClickedLink
+        currentlyActiveTypeLink.setAttribute("class", "list-group-item list-group-item-action"); // removes  active class and keeps the nav-link class on the now old currentlyActiveTypeLink
         currentlyActiveTypeLink = newlyClickedLink; // sets the currentlyActiveTypeLink to be that of the newlyClickedClink
         queryURL = pokemonTypesBaseURL + newlyClickedLink.innerHTML;  // add newlyClickedLink innerHTML to the queryURL
     } else {
@@ -175,13 +208,13 @@ function pokemonListLinkClicked(event) {
     
     if (currentlyActivePokemonLink === undefined) { // check to see if undefined
         // console.log("no current active links, set clicked link active");
-        newlyClickedLink.setAttribute("class", "nav-link text-center active"); // add the active class and keep the nav-link class for the newlyClickedLink
+        newlyClickedLink.setAttribute("class", "list-group-item list-group-item-action active"); // add the active class and keep the nav-link class for the newlyClickedLink
         currentlyActivePokemonLink = newlyClickedLink; // define the currentlyActivePokemonLink to be that of the newlyClickedClink
         queryURL = pokemonBaseURL + newlyClickedLink.innerHTML; //
     } else if (currentlyActivePokemonLink !== newlyClickedLink) {
         // console.log("newly clicked link does not match current active link, set current active to new clicked link");
-        newlyClickedLink.setAttribute("class", "nav-link text-center active"); // add the active class and keep the nav-link class for the newlyClickedLink
-        currentlyActivePokemonLink.setAttribute("class", "nav-link text-center"); // removes the active class and keeps the nav-link class on the now old currentlyActivePokemonLink
+        newlyClickedLink.setAttribute("class", "list-group-item list-group-item-actionr active"); // add the active class and keep the nav-link class for the newlyClickedLink
+        currentlyActivePokemonLink.setAttribute("class", "list-group-item list-group-item-action"); // removes the active class and keeps the nav-link class on the now old currentlyActivePokemonLink
         currentlyActivePokemonLink = newlyClickedLink; // sets the newlyClickedLink as the currentlyActivePokemonLink
         queryURL = pokemonBaseURL + event.target.innerHTML;
     } else {
